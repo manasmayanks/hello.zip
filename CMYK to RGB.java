3683af4d748b8c36bdf30c6c08b8c16184add3c6
Manas Mayank <manasmishra4876@gmail.com>
2025-10-15 16:41:01 +0530

    Create CMYK to RGB.java

---
+++ CMYK to RGB.java
@@ -0,0 +1,14 @@
+public class CMYKtoRGB {
+    public static void main(String[] args) {
+        double c = Double.parseDouble(args[0]);
+        double m = Double.parseDouble(args[1]);
+        double y = Double.parseDouble(args[2]);
+        double k = Double.parseDouble(args[3]);
+
+        int r = (int) Math.round(255 * (1 - c) * (1 - k));
+        int g = (int) Math.round(255 * (1 - m) * (1 - k));
+        int b = (int) Math.round(255 * (1 - y) * (1 - k));
+
+        System.out.println("RGB: (" + r + ", " + g + ", " + b + ")");
+    }
+}
