fe9e409dc9c75ce7006f181019c995ac76d4e067
Manas Mayank <manasmishra4876@gmail.com>
2025-10-15 16:39:06 +0530

    Create RightTraingle.java

---
+++ RightTraingle.java
@@ -0,0 +1,13 @@
+public class RightTriangle {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        boolean isRight = (a * a + b * b == c * c) ||
+                          (a * a + c * c == b * b) ||
+                          (b * b + c * c == a * a);
+
+        System.out.println(isRight);
+    }
+}
